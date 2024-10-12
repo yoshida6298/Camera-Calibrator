190cc697c34d02328f4046116a57f6d371a6b992
Takeshi Yoshida <29244885+yoshida6298@users.noreply.github.com>
2024-10-12 15:12:27 +0900

    関連ファイルの追加

---
+++ camera_calibrator/checker_calib.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+import glob
+import argparse
+import os
+
+
+def checker_calibration(input_dir, checker_size=24.0):
+    extensions = ['jpg', 'jpeg', 'JPG', 'JPEG', 'png']
+    image_paths = []
+    for extension in extensions:
+        found_paths = os.path.join(input_dir, '*.{}'.format(extension))
+        image_paths.extend(glob.glob(found_paths))
+
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    prepare_object_points = np.zeros((6*8, 3), np.float32)
+    prepare_object_points[:, :2] = checker_size*np.mgrid[0:6, 0:8].T.reshape(-1, 2)
+    # Arrays to store object points and image points from all the images.
+    object_points = []  # 3d point in real world space
+    image_points = []  # 2d points in image plane.
+
+    for image_path in image_paths:
+        img = cv2.imread(image_path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (6, 8), None)
+        # If found, add object points, image points (after refining them)
+        if ret:
+            object_points.append(prepare_object_points)
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            image_points.append(corners)
+            # Draw and display the corners
+            cv2.drawChessboardCorners(img, (6, 8), corners2, ret)
+            img2 = cv2.resize(img, (int(img.shape[1]/8), int(img.shape[0]/8)))
+            cv2.imshow('img', img2)
+            cv2.waitKey(500)
+    cv2.destroyAllWindows()
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(object_points, image_points, gray.shape[::-1], None, None)
+    print("!!!!!!")
+    print(dist)
+    np.set_printoptions(precision=2, suppress=True)
+    print(mtx)
+
+
+def main():
+    parser = argparse.ArgumentParser('カメラのキャリブレーション')
+    parser.add_argument('input_dir', help='入力画像のディレクトリ')
+    args = parser.parse_args()
+    checker_calibration(args.input_dir)
+
+
+if __name__ == '__main__':
+    main()
---
+++ camera_calibrator/clip_images.py
@@ -0,0 +1,55 @@
+import os
+import cv2
+import argparse
+import time
+from tqdm import tqdm
+
+
+def clip_all_frames(video_path, output_dir, step, ext='jpg'):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        return
+
+    base_name = os.path.splitext(os.path.basename(video_path))[0]
+    if output_dir:
+        dir_path = output_dir
+    else:
+        dir_path = os.path.join(os.path.dirname(video_path), base_name + "_images")
+    os.makedirs(dir_path, exist_ok=True)
+    base_path = os.path.join(dir_path, base_name)
+
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    digit = len(str(frame_count))
+
+    frame_no = 0
+    start = time.time()
+
+    with tqdm(total=frame_count) as pbar:
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                if frame_no % step == 0:
+                    cv2.imwrite('{}_{}.{}'.format(base_path, str(frame_no).zfill(digit), ext), frame)
+                frame_no += 1
+                pbar.update(1)
+                start = time.time()
+            elif time.time() - start > 20.0:
+                print('Time out')
+                return
+            elif frame_no == frame_count:
+                print('Finished')
+                return
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_path', help='Input movie path')
+    parser.add_argument('--output_dir', default="")
+    parser.add_argument('--step', default=1, type=int)
+    args = parser.parse_args()
+    clip_all_frames(args.input_path, args.output_dir,  args.step)
+
+
+if __name__ == "__main__":
+    main()
